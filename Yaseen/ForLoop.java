157f39db43555cec93162f9a044354737e93f1df
yaseen2050 <arabicinnovation1@gmail.com>
2022-07-17 21:55:01 -0400

    this test

---
+++ Yaseen/ForLoop.java
@@ -0,0 +1,18 @@
+package Yaseen;
+
+public class ForLoop {
+    public static void main(String[] args) {
+
+        int sum = 0 ;
+
+        for (int i = 0; i <=5; i++) {
+
+            System.out.println("i = " + i);
+            sum=sum+i;
+
+            System.out.println("sum = " + sum);
+
+
+        }
+    }
+}
